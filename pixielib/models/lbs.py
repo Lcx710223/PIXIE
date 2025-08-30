29cb23b49c26c0e04ef11cfa81aa0527e0eff35d
LCX@ARGOS <911239555@qq.com>
2025-08-31 02:27:05 +0800

    Update lbs.py

--- pixielib/models/lbs.py
+++ pixielib/models/lbs.py
@@ -1,18 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
-# holder of all proprietary rights on this computer program.
-# You can only use this computer program if you have closed
-# a license agreement with MPG or you get the right to use the computer
-# program from someone who is authorized to grant you that right.
-# Any use of the computer program without a valid license is prohibited and
-# liable to prosecution.
-#
-# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
-# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
-# for Intelligent Systems. All rights reserved.
-#
-# Contact: ps-license@tuebingen.mpg.de
+### JULES20250831-399-增加了LOADER参数以兼容新版的PYYAML。
 
 from __future__ import absolute_import
 from __future__ import print_function
@@ -396,7 +382,7 @@ class JointsFromVerticesSelector(nn.Module):
         if fname is not None:
             fname = os.path.expanduser(os.path.expandvars(fname))
             with open(fname, 'r') as f:
-                data = yaml.load(f, Loader=yaml.FullLoader)
+                data = yaml.load(f, Loader=yaml.FullLoader) # JULES: 添加 Loader 参数以兼容新版 PyYAML
             names = list(data.keys())
             bcs = []
             face_ids = []
