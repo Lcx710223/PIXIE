1ea237b3b799a14b4d5cc84ebe189e1e59204190
LCX@ARGOS <911239555@qq.com>
2025-08-31 02:16:27 +0800

    Update pixie.py

--- pixielib/pixie.py
+++ pixielib/pixie.py
@@ -114,7 +114,7 @@ class PIXIE(object):
         #-- resume model
         model_path = self.cfg.pretrained_modelpath
         if os.path.exists(model_path):
-            checkpoint = torch.load(model_path)
+            checkpoint = torch.load(model_path, map_location=self.device)
             for key in self.model_dict.keys():
                 util.copy_state_dict(self.model_dict[key], checkpoint[key])
         else:
